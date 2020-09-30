0925c876dd5fa2c6d7d8545954483f9f33a5dfe9
Hiroki S <microckey@gmail.com>
2020-09-30 13:28:04 +0900

    test

--- linear_algebra/simple_elim_example.m
+++ linear_algebra/simple_elim_example.m
@@ -32,7 +32,7 @@ disp(x);
 nref=length(b);                %system size for reference problem
 
 %note that the elimination procedure coded below modifies the matrix B
-Awork=cat(2,A,b);          %This is our working version of the matrix used to perform elimination (i.e. it will be modified)
+Awork=cat(2,A,b);        %This is our working version of the matrix used to perform elimination (i.e. it will be modified)
 for ir1=2:nref                                           %loop over rows from 2 to n performing elimination, this index marks what row we are starting the elimination from (i.e. using) for this particular column
     for ir2=ir1:nref                                     %this index marks the present position where elimination is being performed - i.e. where we are applying the elementary row operations
         fact=Awork(ir2,ir1-1);                                    %multiplier of the variable we are attempting to eliminate, its ir-1 column of this row
